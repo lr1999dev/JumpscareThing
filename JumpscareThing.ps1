e6e06f0289c1ec3d81396e02d9317d9385f8f864
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-02 19:39:09 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -3,7 +3,6 @@ public static extern IntPtr GetConsoleWindow();
 
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
-'
 function Hide-Console
 {
     $consolePtr = [Console.Window]::GetConsoleWindow()
