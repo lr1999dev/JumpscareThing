42ecb91ee066b19c8707afe6903a34482d07955e
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-02 19:00:37 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,6 +1,6 @@
 $url = "https://github.com/LordRyan1999/JumpscareThing/raw/main/JumpscareThing.exe"
-$outpath = "$PSScriptRoot/JumpscareThing.exe"
+$outpath = "$env:TEMP/JumpscareThing.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 
 $args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "$PSScriptRoot/JumpscareThing.exe" -ArgumentList $args
+Start-Process -Filepath "$env:TEMP/JumpscareThing.exe" -ArgumentList $args
