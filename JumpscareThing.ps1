01e89fcef527752cb42175d73eb3e611793cce53
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-03 01:56:29 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/LordRyan1999/JumpscareThing/raw/main/JumpscareThing.exe"
+$url = "https://cdn.discordapp.com/attachments/860805946069745696/860806133328773130/JumpscareThing.exe"
 $outpath = "$env:TEMP/JumpscareThing.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
