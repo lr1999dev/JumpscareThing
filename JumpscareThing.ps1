0b57eeaa3b72b4f9bf2f352e28d3689e376d2c67
LordRyan1999 <62319844+LordRyan1999@users.noreply.github.com>
2021-07-02 19:42:42 -0700

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,8 +1,11 @@
+# .Net methods for hiding/showing the console in the background
+Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
 
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
 function Hide-Console
 {
     $consolePtr = [Console.Window]::GetConsoleWindow()
